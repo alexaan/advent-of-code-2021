f7c612a0aa478ba4d7810fb8c4df6d979c270493
Alexander Andersson <aa@driw.no>
2021-12-07 00:09:24 +0100

    add time measurement when running puzzles

--- app/src/main/groovy/no/alexaan/advent/app/days/Day.groovy
+++ app/src/main/groovy/no/alexaan/advent/app/days/Day.groovy
@@ -13,11 +13,15 @@ abstract class Day {
     }
 
     def run() {
-        println "${this.class.simpleName}"
-        println "Part 1"
+        def start = System.currentTimeMillis()
+        println "--- Running ${this.class.simpleName} ---"
+        println "---- Part 1 ----"
         part1()
-        println "Part 2"
+        println "Part 1 finished in ${System.currentTimeMillis() - start}ms"
+        start = System.currentTimeMillis()
+        println "---- Part 2 ----"
         part2()
+        println "Part 2 finished in ${System.currentTimeMillis() - start}ms"
     }
 
     abstract part1()
