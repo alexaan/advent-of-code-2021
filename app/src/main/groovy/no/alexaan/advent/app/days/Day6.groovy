b134156973d921ff28dd4a738f0d32454e44e0bb
Alexander Andersson <aa@driw.no>
2021-12-06 23:45:25 +0100

    add day 6 solution

---
+++ app/src/main/groovy/no/alexaan/advent/app/days/Day6.groovy
@@ -0,0 +1,49 @@
+package no.alexaan.advent.app.days
+
+class Day6 extends Day {
+
+    def part1() {
+        def lines = readFileLineByLine("dec6")
+
+        def fishAgeInput = lines[0].split(",").collect { it.toInteger() }
+        def fishAgeOutput = fishAgeInput.collect()
+
+        (0..79).each { day ->
+            def newFishCount = 0
+            fishAgeOutput = fishAgeOutput.collect { fish ->
+                if (fish == 0) {
+                    newFishCount += 1
+                    6
+                } else {
+                    fish - 1
+                }
+            }
+
+            newFishCount.times { fishAgeOutput.add(8) }
+        }
+
+        println "${fishAgeOutput.size()} fish after 80 days" // 366057
+    }
+
+    def part2() {
+        def lines = readFileLineByLine("dec6")
+
+        def fishAgeInput = lines[0].split(",").collect { it.toInteger() }
+
+        def fishCountByAge = (0..8).collect { age ->
+            fishAgeInput.findAll { it == age }.size().toBigInteger()
+        }
+
+        (0..255).each { day ->
+            def fishCountByAgeCopy = fishCountByAge.collect()
+            (8..0).each { dt ->
+                fishCountByAgeCopy[dt] = fishCountByAge[dt + 1]
+            }
+            fishCountByAgeCopy[6] = fishCountByAgeCopy[6] + fishCountByAge[0]
+            fishCountByAgeCopy[8] = fishCountByAge[0]
+            fishCountByAge = fishCountByAgeCopy
+        }
+
+        println "${fishCountByAge.sum()} fish after 256 days" // 1653559299811
+    }
+}
